0079c21ea635856d08a218a30817673d96d6d000
RakeshSR10 <rakeshsrking@gmail.com>
2021-05-19 17:50:07 +0530

    Added: Ability to Calculating EmpWage for Maximum Hours in Month

--- EmployeeDailyWage.js
+++ EmployeeDailyWage.js
@@ -1,14 +1,22 @@
-//UC4 - Calculating wages for month assuming 20 working Days in a month
+//UC5 - Calculating Employee wage for maximum hours in a month
 //constants
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 2;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 100;
 //variables
-let empHrs = 0;
-//Function call
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+//while loop
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+//Function() call for Get Working Hours of Employee
 function getWorkingHours (empCheck) {
     switch (empCheck) {
         case IS_PART_TIME:
@@ -19,9 +27,5 @@ function getWorkingHours (empCheck) {
             return 0;    
     }
 }
-for(let day = 0; day<NUM_OF_WORKING_DAYS; day++) {
-    let empCheck = Math.floor(Math.random() *10) % 3;
-    empHrs += getWorkingHours(empCheck);
-}
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Employee Hours = " + empHrs + ", Employee Wage: " + empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Emp total days : "+ totalWorkingDays +", Total hours : " + totalEmpHrs +", Employee Wage : "+ empWage);
