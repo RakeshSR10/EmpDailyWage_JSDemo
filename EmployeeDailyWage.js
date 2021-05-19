049dbb27acbce8ad33a8fddeeb009eebabb59482
RakeshSR10 <rakeshsrking@gmail.com>
2021-05-19 17:59:45 +0530

    Added: Ability to Stora Daily wage and Total wage in Array

--- EmployeeDailyWage.js
+++ EmployeeDailyWage.js
@@ -1,4 +1,4 @@
-//UC5 - Calculating Employee wage for maximum hours in a month
+//UC6 - Store Daily Wage Along With Total wage of Employee
 //constants
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -6,17 +6,15 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
-const MAX_HRS_IN_MONTH = 100;
-//variables
+const MAX_HRS_IN_MONTH = 160;
+const MAX_OF_WORKING_DAYS = 20;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
-//while loop
-while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-    totalWorkingDays++;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+//function call
+function calcDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
 }
-//Function() call for Get Working Hours of Employee
+
 function getWorkingHours (empCheck) {
     switch (empCheck) {
         case IS_PART_TIME:
@@ -27,5 +25,15 @@ function getWorkingHours (empCheck) {
             return 0;    
     }
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("Emp total days : "+ totalWorkingDays +", Total hours : " + totalEmpHrs +", Employee Wage : "+ empWage);
+//Array initialization
+let empDailyWageArr = new Array();
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < MAX_OF_WORKING_DAYS)
+       {
+           totalWorkingDays++;
+           let empCheck = Math.floor(Math.random() *10) %3;
+           let empHrs = getWorkingHours(empCheck);
+           totalEmpHrs += empHrs;
+           empDailyWageArr.push(calcDailyWage(empHrs));// push data into array
+       }
+let empWage = calcDailyWage(totalEmpHrs);
+console.log("UC6 Total days = "+ totalWorkingDays + ", Total hrs = "+ totalEmpHrs +", Emp wage = "+ empWage);  
