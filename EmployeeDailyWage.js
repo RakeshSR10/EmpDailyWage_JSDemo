e4b612931f365d8356feadb4029bbaee143cd7fe
RakeshSR10 <rakeshsrking@gmail.com>
2021-05-19 17:43:32 +0530

    Added: Ability Calculating the Employee Monthly Wage

--- EmployeeDailyWage.js
+++ EmployeeDailyWage.js
@@ -1,10 +1,14 @@
-//UC3 - Refactor code to write a function to get work hours
+//UC4 - Calculating wages for month assuming 20 working Days in a month
+//constants
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-//function() call
+const NUM_OF_WORKING_DAYS = 2;
+//variables
+let empHrs = 0;
+//Function call
 function getWorkingHours (empCheck) {
     switch (empCheck) {
         case IS_PART_TIME:
@@ -15,8 +19,9 @@ function getWorkingHours (empCheck) {
             return 0;    
     }
 }
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) %3;
-empHrs = getWorkingHours(empCheck);
+for(let day = 0; day<NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() *10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee wage = "+empWage);
+console.log("Total Employee Hours = " + empHrs + ", Employee Wage: " + empWage);
